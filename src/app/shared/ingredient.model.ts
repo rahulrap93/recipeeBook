47078a8918e24656ba15df9067d61082f6222db7
rahulrap93 <raparti.rahul@gmail.com>
2023-11-24 10:09:53 -0600

    Angular project udemy course part1

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount:number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    //new way of writing this code which will create same effect
+    constructor(public name: string, public amount: number) {}
+}
